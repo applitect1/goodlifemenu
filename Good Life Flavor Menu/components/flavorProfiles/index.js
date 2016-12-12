3ab59e42e6baf99db82b53a27d6b3af70ebde3f5
Chris Fennell <chrisafennell@gmail.com>
2016-12-12 23:09:19 +0000

    Fixed profile navigation

--- Good Life Flavor Menu/components/flavorProfiles/index.js
+++ Good Life Flavor Menu/components/flavorProfiles/index.js
@@ -2,7 +2,14 @@
 
 app.flavorProfiles = kendo.observable({
     onShow: function() {},
-    afterShow: function() {}
+    afterShow: function() {},
+	onClick: function(e) {
+		e.preventDefault();
+		var filtername = $(e.target).data('filtername');
+		app.filtertype = "profile";
+		app.filtername = filtername;
+		app.mobileApp.navigate('components/flavors/view.html');
+	}
 });
 app.localization.registerView('flavorProfiles');
 
